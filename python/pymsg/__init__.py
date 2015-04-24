49a12e3b1a74ba8cb42b08d71508687073189e13
Daniel Maturana <dimatura@cmu.edu>
2015-04-24 05:24:22 -0400

    licensing

--- python/pymsg/__init__.py
+++ python/pymsg/__init__.py
@@ -1,2 +1,13 @@
+#
+# @author  Daniel Maturana
+# @year    2015
+#
+# @attention Copyright (c) 2015
+# @attention Carnegie Mellon University
+# @attention All rights reserved.
+#
+# @=
+
+
 
 from libpymsg import *
--- src_wrap/module.cpp
+++ src_wrap/module.cpp
@@ -1,3 +1,14 @@
+/**
+ * @author  Daniel Maturana
+ * @year    2015
+ *
+ * @attention Copyright (c) 2015
+ * @attention Carnegie Mellon University
+ * @attention All rights reserved.
+ *
+ **@=*/
+
+
 #include <boost/python.hpp>
 #include <boost/shared_ptr.hpp>
 #include <boost/shared_array.hpp>
