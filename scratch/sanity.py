0082a8c9df73dfcbadfd6664f6305ccb608970f2
Daniel Maturana <dimatura@cmu.edu>
2016-12-20 01:10:47 -0500

    converters in header

--- scratch/sanity.py
+++ scratch/sanity.py
@@ -9,6 +9,7 @@ from sensor_msgs.msg import Image
 import cv_bridge
 
 import pymsg
+import pypcd
 
 #ts = pymsg.make_time()
 #print ts
@@ -25,19 +26,19 @@ import pymsg
 #cloud = pymsg.make_pc2(32)
 #print cloud
 #
-#import pypcd
-#
 #print
 
-#pc_data = np.random.random((4, 3)).astype('float32')
-#pc = pypcd.make_xyz_point_cloud(pc_data)
-#print 'pc.width:', pc.width
-#pc_msg = pc.to_msg()
-#
-#print 'pc_msg.width:', pc_msg.width
-#
-#pymsg.print_centroid(pc_msg)
-#print pc_data.mean(0)
+pc_data = np.random.random((4, 3)).astype('float32')
+pc = pypcd.make_xyz_point_cloud(pc_data)
+print 'pc.width:', pc.width
+pc_msg = pc.to_msg()
+print type(pc_msg)
+
+print 'pc_msg.width:', pc_msg.width
+
+pymsg.print_centroid(pc_msg)
+print pc_data.mean(0)
+
 #
 #print 'ci'
 #
@@ -48,14 +49,15 @@ import pymsg
 #print 'img'
 #print
 
-bridge = cv_bridge.CvBridge()
-cv_img = (np.random.random((8, 8))*255).astype('u1')
-img_msg = bridge.cv2_to_imgmsg(cv_img, encoding='passthrough')
-print img_msg
-print '---'
-pymsg.print_img(img_msg)
-
-print '***'
-img_msg2 = pymsg.make_img(8, 8)
-print img_msg2
-cv_img2 = bridge.imgmsg_to_cv2(img_msg2, desired_encoding='passthrough')
+#
+#bridge = cv_bridge.CvBridge()
+#cv_img = (np.random.random((8, 8))*255).astype('u1')
+#img_msg = bridge.cv2_to_imgmsg(cv_img, encoding='passthrough')
+#print img_msg
+#print '---'
+#pymsg.print_img(img_msg)
+#
+#print '***'
+#img_msg2 = pymsg.make_img(8, 8)
+#print img_msg2
+#cv_img2 = bridge.imgmsg_to_cv2(img_msg2, desired_encoding='passthrough')
