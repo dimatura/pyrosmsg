2d0ffe66e0e20801b98b5548e39259986a265838
Daniel Maturana <dimatura@cmu.edu>
2018-04-17 18:07:40 -0400

    moved some stuff

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ from catkin_pkg.python_setup import generate_distutils_setup
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
     packages=['pyrosmsg'],
-    package_dir={'':'python'})
+    package_dir={'':''})
 
 setup(**setup_args)
