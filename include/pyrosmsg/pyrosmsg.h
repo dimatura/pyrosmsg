a6c3e6a09eb457ec629294cafce3ee64230a6c82
Sam Pfeiffer <sammypfeiffer@gmail.com>
2021-03-22 00:23:51 +1100

    Fix extension
    pyrosmsg.h was pointing to files with the wrong extension

--- include/pyrosmsg/pyrosmsg.h
+++ include/pyrosmsg/pyrosmsg.h
@@ -1,7 +1,7 @@
 #ifndef PYROSMSG_H_B5DJTH81
 #define PYROSMSG_H_B5DJTH81
 
-#include <pyrosmsg/converters.h>
-#include <pyrosmsg/serialization.h>
+#include <pyrosmsg/converters.hpp>
+#include <pyrosmsg/serialization.hpp>
 
 #endif /* end of include guard: PYROSMSG_H_B5DJTH81 */
